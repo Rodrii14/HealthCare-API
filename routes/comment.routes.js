9cda2abb71064d2d2017e57ee0515225cbfe910d
Rodrii14 <00087022@uca.edu.sv>
2024-06-21 15:01:25 -0600

    Fix: comment controller

--- routes/comment.routes.js
+++ routes/comment.routes.js
@@ -6,7 +6,7 @@ const validate = require('../validators/index.validators');
 const { auth } = require('../middlewares/auth.middlewares');
 const commentController = require('../controllers/comment.controllers');
 
-router.post('/', auth, create, validate, commentController);
+router.post('/', auth, create, validate, commentController.create);
 router.get('/', auth, commentController.getAll);
 
 module.exports = router;
